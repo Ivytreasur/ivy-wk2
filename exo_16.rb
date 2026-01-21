b70f3dec3e3bde10e62aceac33a1aa2e95bbd3a1
ivy <ivy@DESKTOP-U0O392C>
2026-01-21 20:09:46 +0100

    add exo_16.rb

---
+++ exo_16.rb
@@ -0,0 +1,11 @@
+print "Hi, welcome to my super pyramid! How many levels do you want?"
+print "> "
+levels = gets.chomp.to_i
+
+puts "Here is the pyramid:"
+
+1.upto(levels) do |i|
+  spaces = levels - i
+  hashes = i
+  puts " " * spaces + "#" * hashes
+end
